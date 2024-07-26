9eca65453dc7cd8a55ef8f26e9d86b12efca9241
GokulraajJS <167990155+GokulraajJS@users.noreply.github.com>
2024-07-26 16:24:53 +0530

    Create variables.tf

---
+++ terra/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+   description = "Test region"
+   type = string
+
+}
+variable "ami_ID" {
+   description = "Test AMI_ID"
+   type = string
+
+}
+variable "instance_type"{
+   description = "Test Instnace type"
+   type = string
+
+}
