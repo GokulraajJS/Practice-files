04423de1d3c66212799d210122e520422e2e510e
GokulraajJS <167990155+GokulraajJS@users.noreply.github.com>
2024-07-26 16:14:22 +0530

    Create Guvi.sh

---
+++ Guvi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+num=8
+
+if [ $num -gt 5 ]; then
+
+       echo "$num is greater than 5"
+
+elif [ $num -eq 0 ]; then
+
+        echo "$num is equal to 5"
+
+else
+        echo "$num is less than 5"
+
+fi
